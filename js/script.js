46219d6fddd77480e0aa450bf69fad0aeee3e7c6
elshode <tukhtamurodov.e@gmail.com>
2022-05-02 18:36:12 +0500

    1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const menuOpen = document.querySelectorAll('.menu__open');
+const menu = document.querySelector('.menu');
+if (menuOpen.length > 0) {
+   for (let index = 0; index < menuOpen.length; index++) {
+      const btn = menuOpen[index];
+      btn.addEventListener("click", function (e) {
+         menu.classList.add('_open');
+      })
+   }
+}
+const menuClose = document.querySelectorAll('.menu__close');
+for (let index = 0; index < menuClose.length; index++) {
+   const btn = menuClose[index];
+   btn.addEventListener("click", function (e) {
+
+      if (menu.classList.contains('_open')) {
+         menu.classList.remove('_open');
+      }
+   })
+} 

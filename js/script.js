e80fe10f0ea9658a41d832620342cae682298af4
elshode <tukhtamurodov.e@gmail.com>
2022-05-08 19:15:48 +0500

    v2

--- js/script.js
+++ js/script.js
@@ -3,16 +3,13 @@ new Swiper('.swiper-slide-big', {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
    },
-
    pagination: {
       el: ".swiper-pagination",
-
       clickable: true,
-
    },
    simulateTouch: true,
    grabCursor: true,
-   
+
    keyboard: {
       enable: true,
       onlyInViewport: true,
@@ -38,6 +35,85 @@ new Swiper('.swiper-slide-small', {
       stopOnLastSlide: false,
       disableOnInteraction: true,
    },
+})
+new Swiper('.cards-swiper-slide', {
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   autoplay: {
+      delay: 5000,
+      stopOnLastSlide: false,
+      disableOnInteraction: true,
+   },
+   
+   grabCursor: true,
+   keyboard: {
+      enable: true,
+      onlyInViewport: true,
+   },
+   slidesPerView: 4,
+   watchOverflow: true,
+   slidesPerGroup: 1,
+   centeredSlides: false,
+   speed: 800,
+   loop: true,
+   spaceBetween: 20,
+   breakpoints:{
+      310:{
+         slidesPerView:1,
+      },
+      640:{
+         slidesPerView:2,
+      },
+      1100:{
+         slidesPerView:3,
+      },
+      1400:{
+         slidesPerView:4,
+      }
+   }, 
+})
+
+
+new Swiper('.clients-swiper-slide', {
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   autoplay: {
+      delay: 5000,
+      stopOnLastSlide: false,
+      disableOnInteraction: true,
+   },
+   grabCursor: true,
+   loop: true,
+   slidesPerView: 4,
+   watchOverflow: true,
+   slidesPerGroup: 1,
+   speed: 800,
+   spaceBetween: 20,
+   breakpoints:{
+      310:{
+         slidesPerView:1,
+      },
+      640:{
+         slidesPerView:2,
+      },
+      1100:{
+         slidesPerView:4,
+         allowSlideNext:false,
+         allowSlidePrev:false,
+      },
+   }, 
 })
 
 const menuOpen = document.querySelectorAll('.menu__open');
